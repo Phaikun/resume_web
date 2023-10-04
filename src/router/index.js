d239206330b94b813d55727679fed8c091e82159
Phaikun <carmanze01@gmail.com>
2023-10-04 16:57:33 +0700

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from '../views/HomeView.vue'
 import CreateView from '../views/CreateView.vue'
-import ChatView from '../views/Chatview.vue'
+import ChatView from '../views/ChatView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
